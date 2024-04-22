86f4e0b06aee8a6021a71f165b11609994d91da0
Juan M <juanmanuel2727@gmail.com>
2024-04-22 12:28:21 +0200

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'; // Importa el componente de la página
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige a la ruta del login al inicio
+  { path: 'login', component: LoginComponent }, // Define la ruta para el componente de login
+  //
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { UsuarioService } from 'src/app/core/services/usuario.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +7,4 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(
-    private router: Router,
-    private usuarioService: UsuarioService,
-  ) { }
-
-  async onLogin(farmacia: any){
-    this.usuarioService.comprobarLogin(farmacia).subscribe(
-      (response) => {
-        console.log(response);
-      }
-    );
-  }
 }
