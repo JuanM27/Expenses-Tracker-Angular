22192731ae93407b161170bbda41389cf5011f71
Juan M <juanmanuel2727@gmail.com>
2024-05-14 12:51:59 +0200

    Control de errores login y registro y idUsuario en sessionStorage al iniciar sesion registrarse

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,22 +10,32 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   correo: string = '';
   contrasena: string = '';
+  usuarioContrasenaIncorrecto:boolean = false;
+  mensajeError:string = "";
 
   constructor(
     private router: Router,
     private usuarioService: UsuarioService,
   ) { }
-
-  async onLogin(){
-    this.usuarioService.comprobarLogin(this.correo,this.contrasena).subscribe(
+  
+  async onLogin() {
+    this.usuarioContrasenaIncorrecto=false;
+    this.usuarioService.comprobarLogin(this.correo, this.contrasena).subscribe(
       (response) => {
-        if(response.ok===true&&response.mensaje==="Login correcto"){
-          this.router.navigate(["/home"])
-        }else if(response.ok===false&&response.mensaje==="Error de login, usuario o contraseña incorrecto"){
-          //Poner un div de usuario y contraseña incorrectos
-          console.log("Usuario o contraseña incorrectos")
-        }else{
-          //Poner un div de error de servidor
+        if (response.ok === true && response.mensaje === "Login correcto") {
+          this.router.navigate(["/home"]);
+          sessionStorage.setItem('usuario', response.data.ID_Usuario);
+        }
+      },
+      (error) => {
+        if (error.status === 401) {
+          this.usuarioContrasenaIncorrecto=true;
+          this.mensajeError = 'Usuario y/o contraseña incorrectos';
+        } else {
+          this.usuarioContrasenaIncorrecto=true;
+          console.error('Error al iniciar sesión:', error);
+          // Por ejemplo, podrías mostrar un mensaje genérico al usuario
+          this.mensajeError = 'Error al iniciar sesión. Por favor, inténtelo de nuevo más tarde.';
         }
       }
     );
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -14,37 +14,64 @@ export class RegisterComponent {
   contrasena: string = "";
   confirmarContrasena: string = "";
   objetivo!: number;
-  camposIncompletos: boolean = false;
-  contrasenasNoCoinciden: boolean = false;
+  errorFormato: boolean = false;
+  mensaje: string = "";
+  errorMensaje: string = '';
+  campoError: boolean = false;
 
-  constructor(    
+  constructor(
     private router: Router,
-    private usuarioService: UsuarioService) { 
+    private usuarioService: UsuarioService) {
   }
 
-  async onRegister(){
-    this.camposIncompletos = false; // Reiniciar el valor de camposIncompletos
-    this.contrasenasNoCoinciden = false; // Reiniciar el valor de contrasenasNoCoinciden
+  async onRegister() {
+    this.errorFormato = false; // Reiniciar el valor de camposIncompletos
+    this.campoError = false;
 
     // Verificar si algún campo está vacío
     if (!this.nombre || !this.correo || !this.contrasena || !this.confirmarContrasena) {
-      this.camposIncompletos = true; // Establecer camposIncompletos a true si algún campo está vacío
+      this.errorFormato = true; // Establecer camposIncompletos a true si algún campo está vacío
+      this.mensaje = "Por favor, completa todos los campos.";
       return; // Detener la ejecución de la función
     }
 
     // Verificar si las contraseñas no coinciden
     if (this.contrasena !== this.confirmarContrasena) {
-      this.contrasenasNoCoinciden = true; // Establecer contrasenasNoCoinciden a true si las contraseñas no coinciden
+      this.errorFormato = true; // Establecer contrasenasNoCoinciden a true si las contraseñas no coinciden
+      this.mensaje = "Las constraseñas no coinciden";
       return; // Detener la ejecución de la función
     }
-    console.log(this.contrasena);
 
-    this.usuarioService.registrarUsuario(this.nombre,this.correo,this.contrasena,this.objetivo).subscribe(
+    // Verificar si el correo electrónico tiene un formato válido
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(this.correo)) {
+      this.errorFormato = true; // Establecer errorFormato a true si el formato del correo electrónico es inválido
+      this.mensaje = "Por favor, introduce un correo electrónico válido.";
+      return; // Detener la ejecución de la función
+    }
+
+    this.usuarioService.registrarUsuario(this.nombre, this.correo, this.contrasena, this.objetivo).subscribe(
       (response: any) => {
         console.log(response);
         if (response && response.mensaje === "Usuario creado") {
           this.router.navigate(['/home']); // Redirigir al usuario a /home
+          sessionStorage.setItem('usuario', response.data.ID_Usuario);
         }
-      });
+      },
+      (error) => {
+        if (error.status === 409) {
+          // Aquí manejas el error 409
+          this.campoError = true;
+          // Puedes mostrar un mensaje al usuario, por ejemplo, asignándolo a una variable en tu componente
+          this.errorMensaje = 'Ya existe un usuario registrado con ese correo';
+        } else {
+          // Si hay otro tipo de error, puedes manejarlo aquí
+          console.error('Error al registrar usuario:', error);
+          // También podrías mostrar un mensaje de error genérico al usuario
+          this.errorMensaje = 'Error al registrar usuario. Por favor, inténtelo de nuevo más tarde.';
+        }
+      }
+    );
+
   }
 }
